860a1557d12c956bc2c71ef4191805357dcc7c5f
César Noreña <cesar.norena@ifood.com.br>
2022-06-02 23:54:22 -0300

    Data science: global solution v2

--- data-science/global-solution/eleicoes_presidencias_fisico_v4.sql
+++ data-science/global-solution/eleicoes_presidencias_fisico_v4.sql
@@ -14,8 +14,10 @@ CREATE TABLE eleitor
 (
     id            INTEGER PRIMARY KEY AUTO_INCREMENT,
     id_pessoa     INTEGER,
-    local_votacao TEXT,
-    zona_votacao  TEXT
+    local_votacao TINYTEXT,
+    zona_votacao  TINYTEXT,
+    cidade        TINYTEXT,
+    estado        VARCHAR(2)
 );
 
 CREATE TABLE candidato
@@ -177,7 +179,6 @@ VALUES ('12345678903', 'Pedro', 'Silva', 'masculino', 'Rua Caio Prado, 12'),
        ('12345678249', 'Carlos', 'Do Carmo', 'outro', 'Avenida Brasil, 4523'),
        ('12345678250', 'Carlos', 'Do Carmo', 'outro', 'Avenida Brasil, 4523');
 
-
 SELECT COUNT(id) as pessoa_count
 FROM pessoa;
 
@@ -246,47 +247,47 @@ VALUES ('1234', 1, 1),
 SELECT count(id) AS candidato_count
 FROM candidato;
 
-INSERT INTO eleitor (id_pessoa, local_votacao, zona_votacao)
-VALUES (13, 'Rua da Paz, 125', 4),
-       (14, 'Rua da Paz, 126', 5),
-       (15, 'Rua da Paz, 127', 6),
-       (16, 'Rua da Paz, 128', 7),
-       (17, 'Rua da Paz, 123', 8),
-       (18, 'Rua da Paz, 124', 9),
-       (19, 'Rua da Paz, 129', 10),
-       (20, 'Rua da Paz, 130', 11),
-       (21, 'Rua da Paz, 131', 12),
-       (22, 'Rua da Paz, 132', 13),
-       (23, 'Rua da Paz, 133', 14),
-       (24, 'Rua da Paz, 134', 15),
-       (25, 'Rua da Paz, 135', 16),
-       (26, 'Rua da Paz, 136', 17),
-       (27, 'Rua da Paz, 137', 18),
-       (28, 'Rua da Paz, 138', 19),
-       (29, 'Rua da Paz, 139', 20),
-       (30, 'Rua da Paz, 140', 21),
-       (31, 'Rua da Paz, 141', 22),
-       (32, 'Rua da Paz, 142', 23),
-       (33, 'Rua da Paz, 143', 24),
-       (34, 'Rua da Paz, 144', 25),
-       (35, 'Rua da Paz, 145', 26),
-       (36, 'Rua da Paz, 146', 27),
-       (37, 'Rua da Paz, 147', 28),
-       (38, 'Rua da Paz, 148', 29),
-       (39, 'Rua da Paz, 149', 30),
-       (40, 'Rua da Paz, 150', 31),
-       (41, 'Rua da Paz, 151', 32),
-       (42, 'Rua da Paz, 152', 33),
-       (43, 'Rua da Paz, 153', 34),
-       (44, 'Rua da Paz, 154', 35),
-       (45, 'Rua da Paz, 155', 36),
-       (46, 'Rua da Paz, 156', 37),
-       (47, 'Rua da Paz, 157', 38),
-       (48, 'Rua da Paz, 158', 39),
-       (49, 'Rua da Paz, 159', 40),
-       (50, 'Rua da Paz, 160', 41),
-       (51, 'Rua da Paz, 161', 42),
-       (52, 'Rua da Paz, 162', 43);
+INSERT INTO eleitor (id_pessoa, local_votacao, zona_votacao, cidade, estado)
+VALUES (13, 'Rua da Paz, 125', 4, 'Sao Paulo', 'SP'),
+       (14, 'Rua da Paz, 126', 5, 'Rio de Janeiro', 'RJ'),
+       (15, 'Rua da Paz, 127', 6, 'Rio de Janeiro', 'RJ'),
+       (16, 'Rua da Paz, 128', 7, 'Salvador', 'BA'),
+       (17, 'Rua da Paz, 129', 8, 'Fortaleza', 'CE'),
+       (18, 'Rua da Paz, 130', 9, 'Manaus', 'AM'),
+       (19, 'Rua da Paz, 131', 10, 'Curitiba', 'PR'),
+       (20, 'Rua da Paz, 132', 11, 'Recife', 'PE'),
+       (21, 'Rua da Paz, 133', 12, 'Porto Alegre', 'RS'),
+       (22, 'Rua da Paz, 134', 13, 'Belo Horizonte', 'MG'),
+       (23, 'Rua da Paz, 135', 14, 'Sao Paulo', 'SP'),
+       (24, 'Rua da Paz, 136', 15, 'Rio de Janeiro', 'RJ'),
+       (25, 'Rua da Paz, 137', 16, 'Belo Horizonte', 'MG'),
+       (26, 'Rua da Paz, 138', 17, 'Salvador', 'BA'),
+       (27, 'Rua da Paz, 139', 18, 'Fortaleza', 'CE'),
+       (28, 'Rua da Paz, 140', 19, 'Manaus', 'AM'),
+       (29, 'Rua da Paz, 141', 20, 'Curitiba', 'PR'),
+       (30, 'Rua da Paz, 142', 21, 'Recife', 'PE'),
+       (31, 'Rua da Paz, 143', 22, 'Porto Alegre', 'RS'),
+       (32, 'Rua da Paz, 144', 23, 'Belo Horizonte', 'MG'),
+       (33, 'Rua da Paz, 145', 24, 'Sao Paulo', 'SP'),
+       (34, 'Rua da Paz, 146', 25, 'Rio de Janeiro', 'RJ'),
+       (35, 'Rua da Paz, 147', 26, 'Belo Horizonte', 'MG'),
+       (36, 'Rua da Paz, 148', 27, 'Salvador', 'BA'),
+       (37, 'Rua da Paz, 149', 28, 'Fortaleza', 'CE'),
+       (38, 'Rua da Paz, 150', 29, 'Manaus', 'AM'),
+       (39, 'Rua da Paz, 151', 30, 'Curitiba', 'PR'),
+       (40, 'Rua da Paz, 152', 31, 'Recife', 'PE'),
+       (41, 'Rua da Paz, 153', 32, 'Porto Alegre', 'RS'),
+       (42, 'Rua da Paz, 154', 33, 'Belo Horizonte', 'MG'),
+       (43, 'Rua da Paz, 155', 34, 'Sao Paulo', 'SP'),
+       (44, 'Rua da Paz, 156', 35, 'Rio de Janeiro', 'RJ'),
+       (45, 'Rua da Paz, 157', 36, 'Belo Horizonte', 'MG'),
+       (46, 'Rua da Paz, 158', 37, 'Salvador', 'BA'),
+       (47, 'Rua da Paz, 159', 38, 'Fortaleza', 'CE'),
+       (48, 'Rua da Paz, 160', 39, 'Manaus', 'AM'),
+       (49, 'Rua da Paz, 161', 40, 'Curitiba', 'PR'),
+       (50, 'Rua da Paz, 162', 41, 'Recife', 'PE'),
+       (51, 'Rua da Paz, 163', 42, 'Porto Alegre', 'RS'),
+       (52, 'Rua da Paz, 164', 43, 'Belo Horizonte', 'MG');
 
 SELECT count(id) AS eleitor_count
 FROM eleitor;
@@ -335,38 +336,47 @@ VALUES (1, 1),
 SELECT COUNT(DISTINCT id_coligacao) numero_coligacoes
 FROM coligacao_partido;
 
-
 INSERT INTO voto (id_candidato, id_eleitor)
 VALUES (1, 1),
        (2, 2),
-       (3, 3),
-       (4, 4),
-       (5, 5),
-       (6, 6),
-       (7, 7),
-       (8, 8),
-       (9, 9),
-       (10, 10),
-       (11, 11),
-       (12, 12),
-       (13, 13),
-       (14, 14),
-       (15, 15),
-       (16, 16),
-       (17, 17),
-       (18, 18),
-       (19, 19),
-       (20, 20),
-       (21, 21),
-       (22, 22),
-       (23, 23),
-       (24, 24),
-       (25, 25),
-       (26, 26),
-       (27, 27),
-       (28, 28),
-       (29, 29),
-       (30, 30);
+       (2, 3),
+       (3, 4),
+       (3, 5),
+       (3, 6),
+       (4, 7),
+       (4, 8),
+       (4, 9),
+       (4, 10),
+       (5, 11),
+       (5, 12),
+       (5, 13),
+       (5, 14),
+       (5, 15),
+       (6, 16),
+       (6, 17),
+       (6, 18),
+       (6, 19),
+       (6, 20),
+       (6, 21),
+       (7, 22),
+       (7, 23),
+       (7, 24),
+       (7, 25),
+       (8, 26),
+       (8, 27),
+       (8, 28),
+       (8, 29),
+       (8, 30),
+       (9, 31),
+       (9, 32),
+       (9, 33),
+       (9, 34),
+       (9, 35),
+       (9, 36),
+       (9, 37),
+       (9, 38),
+       (9, 39),
+       (9, 40);
 
 SELECT b.id_partido as id_partido_na_coligacao
 FROM (SELECT id_coligacao FROM coligacao_partido WHERE id_partido = 1) AS a
@@ -390,3 +400,17 @@ FROM partido
          INNER JOIN candidato ON candidato.id_partido = partido.id
          INNER JOIN pessoa ON pessoa.id = candidato.id_pessoa;
 
+SELECT CONCAT(pessoa.nome, ' ', pessoa.sobrenome) AS candidato, SUM(voto.id_candidato) AS votos
+FROM voto
+         INNER JOIN candidato ON voto.id_candidato = candidato.id
+         INNER JOIN pessoa ON candidato.id_pessoa = pessoa.id
+GROUP BY id_candidato
+ORDER BY votos DESC;
+
+SELECT COUNT(voto.id) AS votos, partido.sigla as sigla, eleitor.cidade
+FROM voto
+         INNER JOIN eleitor ON voto.id_eleitor = eleitor.id
+         INNER JOIN candidato ON voto.id_candidato = candidato.id
+         INNER JOIN partido ON candidato.id_partido = partido.id
+GROUP BY sigla, eleitor.cidade
+ORDER BY sigla, votos DESC;
